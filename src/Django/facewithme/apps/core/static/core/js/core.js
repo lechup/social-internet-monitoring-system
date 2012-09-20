307bd5eb86907d88356e2afdadf46a17e36ef509
Leszek Piątek <lpiatek@gmail.com>
2012-09-20 03:42:05 +0200

    dodanie do pracy kilku stron + zmiany js'a i opisu technologii

--- src/Django/facewithme/apps/core/static/core/js/core.js
+++ src/Django/facewithme/apps/core/static/core/js/core.js
@@ -1,4 +1,4 @@
-var map;
+var map = null;
 
 $(document).ready(function() {
 // window resize event
@@ -11,23 +11,15 @@ $(document).ready(function() {
 	function getMapHeight() {
 		return $(window).height()-$('#navbar').height();
 	}
-    
-// map
-    initializeMap();    
+//
 
-	function initializeMap() {
-		var mapOptions = {
-			streetViewControl : false,
-			center: new google.maps.LatLng(0, 0),
-			zoom: 5,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-	
-		map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-		getLatLng(getLatLngHandler);
-	}
-	
-	function getLatLngHandler(pos) {
+// map
+	// initialize map container
+    initializeMap();
+    // get current Lat and Lng of user and set new map center
+	getLatLng(updateMapCenter);
+
+	function updateMapCenter(pos) {
 	// function run after finding current position
 		if (pos.lat && pos.lng) {
 			pos = new google.maps.LatLng(pos.lat, pos.lng);
@@ -35,35 +27,48 @@ $(document).ready(function() {
 			map.panTo(pos);
 		}
 	}
-
-	function getLatLng(callback) {
-		getGeoIPLatLng(callback);
-		// apart from anything
-		// localize through GeoIP
-		
-		if (navigator && navigator.geolocation) {
-		// try to use HTML5 geolocation - it's better than GeoIP
-			navigator.geolocation.getCurrentPosition(function(pos) {
-			// suceess callback
-				callback({
-					'lat' : pos.coords.latitude,
-					'lng' : pos.coords.longitude
-				});
-			}, function(error) {
-				// do nothing because we are already GeoIP'ed
-			});
-		}
-
-		
-		function getGeoIPLatLng(callback) {
-		// helper function, loading MaxMind JS GeoIP and returning lat/lng ...
-			callback({
-				'lat' : geoip_latitude(),
-				'lng' : geoip_longitude()
-			});
-		}
-	}
+//
 });
+
+function initializeMap() {
+	var mapOptions = {
+		streetViewControl : false,
+		center: new google.maps.LatLng(0, 0),
+		zoom: 5,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+}
+
+function getLatLng(callback) {
+	// apart from anything
+	// localize through GeoIP
+	getLatLngGeoIP(callback);
+
+	// try to use HTML5 geolocation - it's better than GeoIP
+	if (navigator && navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			// if user click Yes to 'allow this site my location'
+			callback({
+				'lat' : pos.coords.latitude,
+				'lng' : pos.coords.longitude
+			});
+		}, function(error) {
+			// if user clicked No to 'allow this site my location'
+			// do nothing because we are already GeoIP'ed
+		});
+	}
+	
+	function getLatLngGeoIP(callback) {
+	// helper function, loading MaxMind JS GeoIP and returning lat/lng ...
+		callback({
+			// need MaxMind GeoIP js library!
+			'lat' : geoip_latitude(),
+			'lng' : geoip_longitude()
+		});
+	}
+}
 
 function modal(iframe_src) {
 	$('#modal-iframe').attr('src', iframe_src);
